4e2f9a29eb9d9208cf4facc2ef9469f3b87e3876
ivers1en <stein_iversen@hotmail.com>
2023-05-29 11:47:45 +0200

    commit

--- Teller1til100_sjekker3og5medModulo%.py
+++ Teller1til100_sjekker3og5medModulo%.py
@@ -6,10 +6,10 @@ countersteps = 1
 for i in range(start, end, countersteps):
 
     if i % 3 == 0:
-        print(str(i) + " Fizz")
+        print(str(i) + " Fizz halleluja")
     
     if i % 5 == 0:
-        print(str(i) + " Buzz")
+        print(str(i) + " Buzz madafakka")
 
     else:
         print(i)
